4b1ab98e298d0b3f78cad8dda62833cc23ba351f
hunde02 <matthias.hundeloh@bertelsmann.de>
2023-04-28 16:43:23 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,18 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        path: path.resolve(__dirname, "src/client/build/js"),
+        filename: 'main.js',
+    },
     devtool: 'source-map',
+    mode: 'development',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: 'babel-loader'
             }
         ]
     }
